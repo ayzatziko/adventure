8d681a91cb53afc5e06a581e1a991c7e8d997318
Ayzat <ayzat.ziko.93@gmail.com>
2019-09-15 15:37:09 +0300

    keys do not use graphs

--- keys.go
+++ keys.go
@@ -1,56 +1,21 @@
 package adventure
 
-import (
-	"io"
-
-	"github.com/ayzatziko/algos"
-)
-
 type Keys struct {
-	pos *algos.Graph
-	req *algos.Graph
-}
-
-func ReadKeys(r io.Reader) *Keys {
-	posg := algos.ReadGraph(r)
-	reqg := algos.ReadGraph(r)
-	return &Keys{posg, reqg}
+	nodeKeys map[int][]int
+	req      map[int][]int
 }
 
 func CollectKeys(k *Keys, x int) []int {
-	return algos.Paths(k.pos, x)
+	return k.nodeKeys[x]
 }
 
 func CheckKeys(k *Keys, x int, keys []bool) bool {
-	reqKeys := algos.Paths(k.req, x)
-	if reqKeys == nil {
-		return true
-	}
-
-	var gotKeys []int
-	for key, ok := range keys {
-		if ok {
-			gotKeys = append(gotKeys, key)
+	rkeys := k.req[x]
+	for _, key := range rkeys {
+		if !keys[key] {
+			return false
 		}
 	}
 
-	contains := func(s []int, i int) bool {
-		for _, v := range s {
-			if v == i {
-				return true
-			}
-		}
-
-		return false
-	}
-
-	for range gotKeys {
-		for _, j := range reqKeys {
-			if !contains(gotKeys, j) {
-				return false
-			}
-		}
-	}
-	
 	return true
 }
--- keys_test.go
+++ keys_test.go
@@ -1,16 +1,19 @@
 package adventure
 
 import (
-	"strings"
 	"testing"
-
-	"github.com/ayzatziko/algos"
 )
 
 func TestKeys(t *testing.T) {
-	in := "3 3\n1 0\n3 2\n2 1\n2 3\n4 1\n4 2\n3 0\n"
-	r := strings.NewReader(in)
-	keys := ReadKeys(r)
+	keys := Keys{
+		nodeKeys: map[int][]int{
+			1: []int{0}, 2: []int{1}, 3: []int{2},
+		},
+		req: map[int][]int{
+			4: []int{1, 2},
+			3: []int{0},
+		},
+	}
 
 	collectCases := []struct {
 		x    int
@@ -24,8 +27,8 @@ func TestKeys(t *testing.T) {
 	}
 
 	for i, c := range collectCases {
-		kk := CollectKeys(keys, c.x)
-		if !sameSet(kk, c.keys) {
+		kk := CollectKeys(&keys, c.x)
+		if !same(kk, c.keys) || !same(c.keys, kk) {
 			t.Fatalf("%d: expected %v is equal to %v\n", i, kk, c.keys)
 		}
 	}
@@ -50,28 +53,21 @@ func TestKeys(t *testing.T) {
 	}
 
 	for i, c := range checkCases {
-		ok := CheckKeys(keys, c.x, c.keys)
+		ok := CheckKeys(&keys, c.x, c.keys)
 		if ok != c.ok {
 			if !c.ok {
-				t.Fatalf("%d: expected failure, x: %d, keys: %v, g: %s\n", i, c.x, c.keys, algos.String(keys.req))
+				t.Fatalf("%d: expected failure, x: %d, keys: %v, g: %v\n", i, c.x, c.keys, keys.req)
 			} else {
-				t.Fatalf("%d: expected success, x: %d, keys: %v, g: %s\n", i, c.x, c.keys, algos.String(keys.req))
+				t.Fatalf("%d: expected success, x: %d, keys: %v, g: %v\n", i, c.x, c.keys, keys.req)
 			}
 		}
 	}
 }
 
-// sameSet returns true if the first slice contains all elements from the second
-func sameSet(a, b []int) bool {
-	if a == nil && b == nil {
-		return true
-	} else if len(a) != len(b) {
-		return false
-	}
-
-	contains := func(s []int, i int) bool {
-		for _, v := range s {
-			if v == i {
+func same(a, b []int) bool {
+	containes := func(ints []int, v int) bool {
+		for _, i := range ints {
+			if i == v {
 				return true
 			}
 		}
@@ -79,19 +75,9 @@ func sameSet(a, b []int) bool {
 		return false
 	}
 
-	for range a {
-		for _, j := range b {
-			if !contains(a, j) {
-				return false
-			}
-		}
-	}
-
-	for range b {
-		for _, j := range a {
-			if !contains(b, j) {
-				return false
-			}
+	for _, av := range a {
+		if !containes(b, av) {
+			return false
 		}
 	}
 
